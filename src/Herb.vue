55cfb663e865270578d2f2ae01a34836aa9c4607
lianghuiyang <seatangle@outlook.com>
2024-03-29 12:19:12 +0800

    1

--- src/Herb.vue
+++ src/Herb.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
       <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  
+      <h1>Herb</h1>
       <div class="wrapper">
         <HelloWorld msg="Herb" />
       </div>
